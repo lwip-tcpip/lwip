7d16087789fc39f690b60855b04ef86a9bfdcb8b
christiaans <christiaans>
2006-08-17 09:40:15 +0000

    Fixed bug #17200, added check for broadcast destinations for PCBs bound to a unicast address.

--- src/core/udp.c
+++ src/core/udp.c
@@ -145,7 +145,8 @@ udp_input(struct pbuf *p, struct netif *inp)
     /* compare PCB local addr+port to UDP destination addr+port */
     if ((pcb->local_port == dest) &&
         (ip_addr_isany(&pcb->local_ip) ||
-         ip_addr_cmp(&(pcb->local_ip), &(iphdr->dest)))) {
+         ip_addr_cmp(&(pcb->local_ip), &(iphdr->dest)) || 
+         ip_addr_isbroadcast(&(iphdr->dest), inp))) {
       local_match = 1;
       if ((uncon_pcb == NULL) && 
           ((pcb->flags & UDP_FLAGS_CONNECTED) == 0)) {
