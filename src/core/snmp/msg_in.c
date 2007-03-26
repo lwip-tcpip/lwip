4aee4c1ac70d7c72c084c436f42f1d3823842999
goldsimon <goldsimon>
2007-03-26 19:27:11 +0000

    Free pbuf if pbuf_header() fails

--- src/core/snmp/msg_in.c
+++ src/core/snmp/msg_in.c
@@ -805,6 +805,7 @@ snmp_recv(void *arg, struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, 
   /* peek in the UDP header (goto IP payload) */
   if(pbuf_header(p, UDP_HLEN)){
     LWIP_ASSERT("Can't move to UDP header", 0);
+    pbuf_free(p);
     return;
   }
   udphdr = p->payload;
