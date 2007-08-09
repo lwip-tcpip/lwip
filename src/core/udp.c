118331207b8404904184897d81c1ec77fe0fb5c4
fbernon <fbernon>
2007-08-09 18:34:03 +0000

    udp.c: Fix for bug #20503 : IGMP Improvement. Enable to receive a multicast packet on a udp pcb binded on an netif's IP address, and not on "any".

--- src/core/udp.c
+++ src/core/udp.c
@@ -154,6 +154,9 @@ udp_input(struct pbuf *p, struct netif *inp)
     if ((pcb->local_port == dest) &&
         (ip_addr_isany(&pcb->local_ip) ||
          ip_addr_cmp(&(pcb->local_ip), &(iphdr->dest)) || 
+#if LWIP_IGMP
+         ip_addr_ismulticast(&(iphdr->dest)) ||
+#endif /* LWIP_IGMP */
          ip_addr_isbroadcast(&(iphdr->dest), inp))) {
       local_match = 1;
       if ((uncon_pcb == NULL) && 
