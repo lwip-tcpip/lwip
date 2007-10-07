911ee4d9f9e3f9c3b5f867d9d56c4f5c0270fbf0
fbernon <fbernon>
2007-10-07 17:41:21 +0000

    Minor change (coding style)

--- src/core/init.c
+++ src/core/init.c
@@ -93,6 +93,9 @@
 #if (LWIP_TCP && (TCP_WND > 0xffff))
   #error "If you want to use TCP, TCP_WND must fit in an u16_t, so, you have to reduce it in your lwipopts.h"
 #endif
+#if (LWIP_TCP && (TCP_SND_QUEUELEN > 0xffff))
+  #error If you want to use TCP, TCP_SND_QUEUELEN must fit in an u16_t", so, you have to reduce it in your lwipopts.h"
+#endif
 #if (LWIP_IGMP && (MEMP_NUM_IGMP_GROUP<=1))
   #error "If you want to use IGMP, you have to define MEMP_NUM_IGMP_GROUP>1 in your lwipopts.h"
 #endif
@@ -128,9 +131,6 @@
 #endif
 #if (LWIP_TCP && ((LWIP_EVENT_API && LWIP_CALLBACK_API) || (!LWIP_EVENT_API && !LWIP_CALLBACK_API)))
   #error "One and exactly one of LWIP_EVENT_API and LWIP_CALLBACK_API has to be enabled in lwipopts.h"
-#endif
-#if TCP_SND_QUEUELEN > 0xffff
-  #error TCP_SND_QUEUELEN must fit in an u16_t
 #endif
 /* There must be sufficient timeouts, taking into account requirements of the subsystems. */
 #if ((NO_SYS==0) && (MEMP_NUM_SYS_TIMEOUT < (LWIP_TCP + IP_REASSEMBLY + LWIP_ARP + (2*LWIP_DHCP) + LWIP_AUTOIP + LWIP_IGMP + LWIP_PPP)))
