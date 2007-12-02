e4590b26fb6d2ec509a3823036e6fc3ea3478828
fbernon <fbernon>
2007-12-02 23:33:58 +0000

    Include a temporary sample of queue.h to fix build.

--- src/netif/ppp/ppp_oe.c
+++ src/netif/ppp/ppp_oe.c
@@ -80,10 +80,91 @@
 #include "netif/ppp_oe.h"
 #include "netif/etharp.h"
 
-#include "queue.h"
-
 #include <string.h>
 #include <stdio.h>
+
+/** @todo Replace this part with a simple list like other lwIP lists */
+#ifndef _SYS_QUEUE_H_
+#define _SYS_QUEUE_H_
+
+/*
+ * A list is headed by a single forward pointer (or an array of forward
+ * pointers for a hash table header). The elements are doubly linked
+ * so that an arbitrary element can be removed without a need to
+ * traverse the list. New elements can be added to the list before
+ * or after an existing element or at the head of the list. A list
+ * may only be traversed in the forward direction.
+ *
+ * For details on the use of these macros, see the queue(3) manual page.
+ */
+
+/*
+ * List declarations.
+ */
+#define  LIST_HEAD(name, type)                                                 \
+struct name {                                                                  \
+  struct type *lh_first;  /* first element */                                  \
+}
+
+#define  LIST_HEAD_INITIALIZER(head)                                           \
+  { NULL }
+
+#define  LIST_ENTRY(type)                                                      \
+struct {                                                                       \
+  struct type *le_next;  /* next element */                                    \
+  struct type **le_prev; /* address of previous next element */                \
+}
+
+/*
+ * List functions.
+ */
+
+#define  LIST_EMPTY(head)  ((head)->lh_first == NULL)
+
+#define  LIST_FIRST(head)  ((head)->lh_first)
+
+#define  LIST_FOREACH(var, head, field)                                        \
+  for ((var) = LIST_FIRST((head));                                             \
+      (var);                                                                   \
+      (var) = LIST_NEXT((var), field))
+
+#define  LIST_INIT(head) do {                                                  \
+  LIST_FIRST((head)) = NULL;                                                   \
+} while (0)
+
+#define  LIST_INSERT_AFTER(listelm, elm, field) do {                           \
+  if ((LIST_NEXT((elm), field) = LIST_NEXT((listelm), field)) != NULL)         \
+    LIST_NEXT((listelm), field)->field.le_prev =                               \
+        &LIST_NEXT((elm), field);                                              \
+  LIST_NEXT((listelm), field) = (elm);                                         \
+  (elm)->field.le_prev = &LIST_NEXT((listelm), field);                         \
+} while (0)
+
+#define  LIST_INSERT_BEFORE(listelm, elm, field) do {                          \
+  (elm)->field.le_prev = (listelm)->field.le_prev;                             \
+  LIST_NEXT((elm), field) = (listelm);                                         \
+  *(listelm)->field.le_prev = (elm);                                           \
+  (listelm)->field.le_prev = &LIST_NEXT((elm), field);                         \
+} while (0)
+
+#define  LIST_INSERT_HEAD(head, elm, field) do {                               \
+  if ((LIST_NEXT((elm), field) = LIST_FIRST((head))) != NULL)                  \
+    LIST_FIRST((head))->field.le_prev = &LIST_NEXT((elm), field);              \
+  LIST_FIRST((head)) = (elm);                                                  \
+  (elm)->field.le_prev = &LIST_FIRST((head));                                  \
+} while (0)
+
+#define  LIST_NEXT(elm, field)  ((elm)->field.le_next)
+
+#define  LIST_REMOVE(elm, field) do {                                          \
+  if (LIST_NEXT((elm), field) != NULL)                                         \
+    LIST_NEXT((elm), field)->field.le_prev =                                   \
+        (elm)->field.le_prev;                                                  \
+  *(elm)->field.le_prev = LIST_NEXT((elm), field);                             \
+} while (0)
+
+#endif /* !_SYS_QUEUE_H_ */
+
 
 /* Add a 16 bit unsigned value to a buffer pointed to by PTR */
 #define	PPPOE_ADD_16(PTR, VAL)			\
