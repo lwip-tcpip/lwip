22f51f953b7503b84e9f50134368e9464843f73a
goldsimon <goldsimon>
2007-12-03 18:01:28 +0000

    Added short comment in file description about how to handle MEM_USE_POOLS / MEM_LIBC_MALLOC

--- src/core/mem.c
+++ src/core/mem.c
@@ -2,6 +2,22 @@
  * @file
  * Dynamic memory manager
  *
+ * This is a lightweight replacement for the standard C library malloc().
+ *
+ * If you want to use the standard C library malloc() instead, define
+ * MEM_LIBC_MALLOC to 1 in your lwipopts.h
+ *
+ * To let mem_malloc() use pools (prevents fragmentation and is much faster than
+ * a heap but might waste some memory), define MEM_USE_POOLS to 1, define
+ * MEM_USE_CUSTOM_POOLS to 1 and create a file "lwippools.h" that includes a list
+ * of pools like the this (more pools can be added between _START and _END):
+ *
+ * /* Define three pools with sizes 256, 512, and 1512 bytes
+ * LWIP_MALLOC_MEMPOOL_START
+ * LWIP_MALLOC_MEMPOOL(20, 256)
+ * LWIP_MALLOC_MEMPOOL(10, 512)
+ * LWIP_MALLOC_MEMPOOL(5, 1512)
+ * LWIP_MALLOC_MEMPOOL_END
  */
 
 /*
