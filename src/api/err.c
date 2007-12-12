1dd10640516465787476700565eab1d41bf75d11
fbernon <fbernon>
2007-12-12 08:25:23 +0000

    Fix err_stderr[] table with new err.h defines.

--- src/api/err.c
+++ src/api/err.c
@@ -44,13 +44,13 @@ static const char *err_strerr[] = {
            "Ok.",                    /* ERR_OK       0  */
            "Out of memory error.",   /* ERR_MEM     -1  */
            "Buffer error.",          /* ERR_BUF     -2  */
-           "Connection aborted.",    /* ERR_ABRT    -3  */
-           "Connection reset.",      /* ERR_RST     -4  */
-           "Connection closed.",     /* ERR_CLSD    -5  */
-           "Not connected.",         /* ERR_CONN    -6  */
-           "Illegal value.",         /* ERR_VAL     -7  */
-           "Illegal argument.",      /* ERR_ARG     -8  */
-           "Routing problem.",       /* ERR_RTE     -9  */
+           "Routing problem.",       /* ERR_RTE     -3  */
+           "Connection aborted.",    /* ERR_ABRT    -4  */
+           "Connection reset.",      /* ERR_RST     -5  */
+           "Connection closed.",     /* ERR_CLSD    -6  */
+           "Not connected.",         /* ERR_CONN    -7  */
+           "Illegal value.",         /* ERR_VAL     -8  */
+           "Illegal argument.",      /* ERR_ARG     -9  */
            "Address in use.",        /* ERR_USE     -10 */
            "Low-level netif error.", /* ERR_IF      -11 */
            "Already connected.",     /* ERR_ISCONN  -12 */
