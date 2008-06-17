460b18e82c4d15116a7a32968d4a84a43b67a032
goldsimon <goldsimon>
2008-06-17 20:12:22 +0000

    Fixed loopif_poll define (now maps directly to netif_poll)

--- src/include/netif/loopif.h
+++ src/include/netif/loopif.h
@@ -32,6 +32,7 @@
 #ifndef __NETIF_LOOPIF_H__
 #define __NETIF_LOOPIF_H__
 
+#include "lwip/opt.h"
 #include "lwip/netif.h"
 #include "lwip/err.h"
 
@@ -40,8 +41,8 @@ extern "C" {
 #endif
 
 #if !LWIP_NETIF_LOOPBACK_MULTITHREADING
-#define loopif_poll(netif) netif_poll()
-#endif
+#define loopif_poll netif_poll
+#endif /* !LWIP_NETIF_LOOPBACK_MULTITHREADING */
 
 err_t loopif_init(struct netif *netif);
 
