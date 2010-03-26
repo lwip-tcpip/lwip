4b038f13dece49f84df5da9604c091db81e678f8
goldsimon <goldsimon>
2010-03-26 14:04:03 +0000

    sys_thread_new: name is a const pointer

--- src/include/lwip/sys.h
+++ src/include/lwip/sys.h
@@ -215,7 +215,7 @@ void sys_mbox_set_invalid(sys_mbox_t *mbox);
  * @param arg parameter passed to 'thread'
  * @param stacksize stack size in bytes for the new thread (may be ignored by ports)
  * @param prio priority of the new thread (may be ignored by ports) */
-sys_thread_t sys_thread_new(char *name, lwip_thread_fn thread, void *arg, int stacksize, int prio);
+sys_thread_t sys_thread_new(const char *name, lwip_thread_fn thread, void *arg, int stacksize, int prio);
 
 #endif /* NO_SYS */
 
