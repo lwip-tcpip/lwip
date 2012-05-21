d0645273df2c6b64e518a7ac8b6093a15f476aa0
Sylvain Rochet <gradator@gradator.net>
2012-05-21 23:02:27 +0200

    removed HMAC support from ciphers

--- src/netif/ppp/polarssl/md5.h
+++ src/netif/ppp/polarssl/md5.h
@@ -43,9 +43,6 @@ typedef struct
     unsigned long total[2];     /*!< number of bytes processed  */
     unsigned long state[4];     /*!< intermediate digest state  */
     unsigned char buffer[64];   /*!< data block being processed */
-
-    unsigned char ipad[64];     /*!< HMAC: inner padding        */
-    unsigned char opad[64];     /*!< HMAC: outer padding        */
 }
 md5_context;
 
