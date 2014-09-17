aa6f6bc3aa3699859ec29605c455f1879b327af4
Simon Goldschmidt <goldsimon@gmx.de>
2014-09-17 22:52:58 +0200

    patch #8480 Fix handling of dns_seqno wraparound

--- src/core/dns.c
+++ src/core/dns.c
@@ -1161,7 +1161,7 @@ dns_enqueue(const char *name, size_t hostnamelen, dns_found_callback found,
     }
     /* check if this is the oldest completed entry */
     if (entry->state == DNS_STATE_DONE) {
-      if ((dns_seqno - entry->seqno) > lseq) {
+      if ((u8_t)(dns_seqno - entry->seqno) > lseq) {
         lseq = dns_seqno - entry->seqno;
         lseqi = i;
       }
