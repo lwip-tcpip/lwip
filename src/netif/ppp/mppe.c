435a0b98ac561a05c168f7adcd79115e53118e53
Sylvain Rochet <gradator@gradator.net>
2015-03-26 23:54:09 +0100

    PPP, MPPE, cleaning license notice
    We only need to keep the BSD license part in lwIP, cleaning the part which
    is only relevant for the Linux kernel.

--- src/netif/ppp/mppe.c
+++ src/netif/ppp/mppe.c
@@ -1,6 +1,5 @@
 /*
- * ppp_mppe.c - interface MPPE to the PPP code.
- * This version is for use with Linux kernel 2.6.14+
+ * mppe.c - interface MPPE to the PPP code.
  *
  * By Frank Cusack <fcusack@fcusack.com>.
  * Copyright (c) 2002,2003,2004 Google, Inc.
@@ -11,24 +10,6 @@
  * documentation is hereby granted, provided that the above copyright
  * notice appears in all copies.  This software is provided without any
  * warranty, express or implied.
- *
- * ALTERNATIVELY, provided that this notice is retained in full, this product
- * may be distributed under the terms of the GNU General Public License (GPL),
- * in which case the provisions of the GPL apply INSTEAD OF those given above.
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, see <http://www.gnu.org/licenses/>.
- *
  *
  * Changelog:
  *      08/12/05 - Matt Domsch <Matt_Domsch@dell.com>
