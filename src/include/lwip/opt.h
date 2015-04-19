f4ef6180b82b9edd63e4ec138a1390a499757280
Sylvain Rochet <gradator@gradator.net>
2015-04-19 16:28:48 +0200

    PPP, MPPE: allow users to enable CCP and MPPE support in opt.h
    We are new ready to process CCP and MPPE packets, allow lwIP users to benefit
    from those features.

--- src/include/lwip/opt.h
+++ src/include/lwip/opt.h
@@ -2054,24 +2054,28 @@
 #endif
 
 /**
- * CBCP_SUPPORT==1: Support CBCP. CURRENTLY NOT SUPPORTED! DO NOT SET!
- */
-#ifndef CBCP_SUPPORT
-#define CBCP_SUPPORT                    0
-#endif
-
-/**
- * CCP_SUPPORT==1: Support CCP. CURRENTLY NOT SUPPORTED! DO NOT SET!
+ * CCP_SUPPORT==1: Support CCP.
  */
 #ifndef CCP_SUPPORT
 #define CCP_SUPPORT                     0
 #endif
 
 /**
- * MPPE_SUPPORT==1: Support MPPE. CURRENTLY NOT SUPPORTED! DO NOT SET!
+ * MPPE_SUPPORT==1: Support MPPE.
  */
 #ifndef MPPE_SUPPORT
-#define MPPE_SUPPORT                     0
+#define MPPE_SUPPORT                    0
+#endif
+#if MPPE_SUPPORT
+#undef CCP_SUPPORT
+#define CCP_SUPPORT                     1   /* MPPE requires CCP support */
+#endif /* MPPE_SUPPORT */
+
+/**
+ * CBCP_SUPPORT==1: Support CBCP. CURRENTLY NOT SUPPORTED! DO NOT SET!
+ */
+#ifndef CBCP_SUPPORT
+#define CBCP_SUPPORT                    0
 #endif
 
 /**
