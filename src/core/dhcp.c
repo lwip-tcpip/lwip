d3217718a904ffd3b6f6cd12d14d526813d46d7a
goldsimon <goldsimon@gmx.de>
2015-06-23 09:20:06 +0200

    Reverted fix for bug #38203 since it might be wrong

--- src/core/dhcp.c
+++ src/core/dhcp.c
@@ -1659,7 +1659,6 @@ dhcp_recv(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr,
     /* already bound to the given lease address? */
     else if ((dhcp->state == DHCP_STATE_REBOOTING) || (dhcp->state == DHCP_STATE_REBINDING) ||
              (dhcp->state == DHCP_STATE_RENEWING)) {
-      dhcp_handle_ack(netif);
       dhcp_bind(netif);
     }
   }
