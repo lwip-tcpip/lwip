94e502fc8adfc23e0f5fc16f07f99c7bbb3c5464
Erik Ekman <erik@kryo.se>
2016-06-24 09:16:56 +0200

    Switch to C-style comments in test lwipopts

--- test/unit/lwipopts.h
+++ test/unit/lwipopts.h
@@ -48,7 +48,7 @@
 #define TCP_WND                         (10 * TCP_MSS)
 #define LWIP_WND_SCALE                  1
 #define TCP_RCV_SCALE                   0
-#define PBUF_POOL_SIZE                  400 // pbuf tests need ~200KByte
+#define PBUF_POOL_SIZE                  400 /* pbuf tests need ~200KByte */
 
 /* Minimal changes to opt.h required for etharp unit tests: */
 #define ETHARP_SUPPORT_STATIC_ENTRIES   1
