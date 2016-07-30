6cc7f38c99e0a58e1e34c8b9962911c5f4360c19
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2016-07-30 11:08:05 +0200

    Fix typo in lwip.Doxyfile

---
+++ doc/doxygen/doxygenopts.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   doxygenopts.h
+ * Author: dziegel
+ *
+ * Created on July 30, 2016, 11:06 AM
+ */
+
+#ifndef DOXYGENOPTS_H
+#define DOXYGENOPTS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DOXYGENOPTS_H */
+
