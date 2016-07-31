e76b8b2551d53e19aa35e61ed95277aef5767310
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2016-07-31 16:02:22 +0200

    Document SNMP MIB2 netif stats counters

--- src/include/lwip/stats.h
+++ src/include/lwip/stats.h
@@ -179,47 +179,50 @@ struct stats_mib2 {
   u32_t icmpoutechoreps;
 };
 
-/** SNMP MIB2 interface stats */
+/**
+ * @ingroup netif_mib2
+ * SNMP MIB2 interface stats
+ */
 struct stats_mib2_netif_ctrs {
-  /* The total number of octets received on the interface, including framing characters */
+  /** The total number of octets received on the interface, including framing characters */
   u32_t ifinoctets;
-  /* The number of packets, delivered by this sub-layer to a higher (sub-)layer, which were
+  /** The number of packets, delivered by this sub-layer to a higher (sub-)layer, which were
    * not addressed to a multicast or broadcast address at this sub-layer */
   u32_t ifinucastpkts;
-  /* The number of packets, delivered by this sub-layer to a higher (sub-)layer, which were 
+  /** The number of packets, delivered by this sub-layer to a higher (sub-)layer, which were 
    * addressed to a multicast or broadcast address at this sub-layer */
   u32_t ifinnucastpkts;
-  /* The number of inbound packets which were chosen to be discarded even though no errors had 
+  /** The number of inbound packets which were chosen to be discarded even though no errors had 
    * been detected to prevent their being deliverable to a higher-layer protocol. One possible 
    * reason for discarding such a packet could be to free up buffer space */
   u32_t ifindiscards;
-  /* For packet-oriented interfaces, the number of inbound packets that contained errors 
+  /** For packet-oriented interfaces, the number of inbound packets that contained errors 
    * preventing them from being deliverable to a higher-layer protocol.  For character-
    * oriented or fixed-length interfaces, the number of inbound transmission units that 
    * contained errors preventing them from being deliverable to a higher-layer protocol. */
   u32_t ifinerrors;
-  /* For packet-oriented interfaces, the number of packets received via the interface which
+  /** For packet-oriented interfaces, the number of packets received via the interface which
    * were discarded because of an unknown or unsupported protocol.  For character-oriented
    * or fixed-length interfaces that support protocol multiplexing the number of transmission
    * units received via the interface which were discarded because of an unknown or unsupported
    * protocol. For any interface that does not support protocol multiplexing, this counter will
    * always be 0 */
   u32_t ifinunknownprotos;
-  /* The total number of octets transmitted out of the interface, including framing characters. */
+  /** The total number of octets transmitted out of the interface, including framing characters. */
   u32_t ifoutoctets;
-  /* The total number of packets that higher-level protocols requested be transmitted, and
+  /** The total number of packets that higher-level protocols requested be transmitted, and
    * which were not addressed to a multicast or broadcast address at this sub-layer, including
    * those that were discarded or not sent. */
   u32_t ifoutucastpkts;
-  /* The total number of packets that higher-level protocols requested be transmitted, and which
+  /** The total number of packets that higher-level protocols requested be transmitted, and which
    * were addressed to a multicast or broadcast address at this sub-layer, including
    * those that were discarded or not sent. */
   u32_t ifoutnucastpkts;
-  /* The number of outbound packets which were chosen to be discarded even though no errors had
+  /** The number of outbound packets which were chosen to be discarded even though no errors had
    * been detected to prevent their being transmitted.  One possible reason for discarding
    * such a packet could be to free up buffer space. */
   u32_t ifoutdiscards;
-  /* For packet-oriented interfaces, the number of outbound packets that could not be transmitted
+  /** For packet-oriented interfaces, the number of outbound packets that could not be transmitted
    * because of errors. For character-oriented or fixed-length interfaces, the number of outbound
    * transmission units that could not be transmitted because of errors. */
   u32_t ifouterrors;
