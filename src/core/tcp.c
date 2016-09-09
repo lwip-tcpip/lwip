8cd43a3dd9fc839be6a66da9cef8bdb271b2cc1d
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2016-09-09 17:36:49 +0200

    Documentation: Add one more note PCB is already freed in tcp_err callback.

--- src/core/tcp.c
+++ src/core/tcp.c
@@ -1681,6 +1681,8 @@ tcp_sent(struct tcp_pcb *pcb, tcp_sent_fn sent)
  * Used to specify the function that should be called when a fatal error
  * has occurred on the connection.
  *
+ * @note The corresponding pcb is already freed when this callback is called!
+ * 
  * @param pcb tcp_pcb to set the err callback
  * @param err callback function to call for this pcb when a fatal error
  *        has occurred on the connection
