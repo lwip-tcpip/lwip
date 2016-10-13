760281207ede6225971ef39ce42f1cc56d1d2af0
David van Moolenbroek <david@minix3.org>
2016-10-13 16:30:32 +0000

    mld6: fix conditional checksumming
    The mld_group structure no longer has a 'netif' field, as such
    structures are now linked from the corresponding netif structure.
    For conditional checksumming, use the calling function's netif
    reference instead.

--- src/core/ipv6/mld6.c
+++ src/core/ipv6/mld6.c
@@ -564,7 +564,7 @@ mld6_send(struct netif *netif, struct mld_group *group, u8_t type)
   ip6_addr_set(&(mld_hdr->multicast_address), &(group->group_address));
 
 #if CHECKSUM_GEN_ICMP6
-  IF__NETIF_CHECKSUM_ENABLED(group->netif, NETIF_CHECKSUM_GEN_ICMP6) {
+  IF__NETIF_CHECKSUM_ENABLED(netif, NETIF_CHECKSUM_GEN_ICMP6) {
     mld_hdr->chksum = ip6_chksum_pseudo(p, IP6_NEXTH_ICMP6, p->len,
       src_addr, &(group->group_address));
   }
