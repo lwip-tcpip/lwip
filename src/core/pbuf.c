801f26ee0c4fac8bfda52d0a8707ee8ba72efacc
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2016-10-16 19:07:55 +0200

    Minor documentation update

--- src/core/pbuf.c
+++ src/core/pbuf.c
@@ -41,17 +41,22 @@ typedef struct my_custom_pbuf
    void* dma_descriptor;
 } my_custom_pbuf_t;
 
+LWIP_MEMPOOL_DECLARE(RX_POOL, 10, sizeof(my_custom_pbuf_t), "Zero-copy RX PBUF pool");
+
 void my_pbuf_free_custom(void* p)
 {
   my_custom_pbuf_t* my_puf = (my_custom_pbuf_t*)p;
+
+  LOCK_INTERRUPTS();
   free_rx_dma_descriptor(my_pbuf->dma_descriptor);
-  my_pbuf_pool_put(my_pbuf);
+  LWIP_MEMPOOL_FREE(RX_POOL, my_pbuf);
+  UNLOCK_INTERRUPTS();
 }
 
 void eth_rx_irq()
 {
   dma_descriptor*   dma_desc = get_RX_DMA_descriptor_from_ethernet();
-  my_custom_pbuf_t* my_pbuf  = my_pbuf_pool_get();
+  my_custom_pbuf_t* my_pbuf  = (my_custom_pbuf_t*)LWIP_MEMPOOL_ALLOC(RX_POOL);
 
   my_pbuf->p.custom_free_function = my_pbuf_free_custom;
   my_pbuf->dma_descriptor         = dma_desc;
