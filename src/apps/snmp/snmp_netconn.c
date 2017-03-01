f8f3cc039acc1fd1273d4b1e1a8b3c9f2d35bf23
Marco <marco.veeneman@schleifenbauer.eu>
2017-03-01 16:18:51 +0100

    Start a timer for enginetime handling.
    This timer function should be implemented in the snmpv3_xxx.c file.

--- src/apps/snmp/snmp_netconn.c
+++ src/apps/snmp/snmp_netconn.c
@@ -64,6 +64,11 @@ snmp_netconn_thread(void *arg)
   
   snmp_traps_handle = conn;
 
+#if LWIP_SNMP_V3
+  /* Start the engine time timer */
+  tcpip_enginetime_timer(NULL);
+#endif
+
   do {
     err = netconn_recv(conn, &buf);
 
