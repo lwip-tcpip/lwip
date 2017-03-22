ba20cd229e3ea758c0f09a5f66c1e62d68247895
goldsimon <goldsimon@gmx.de>
2017-03-22 21:12:09 +0100

    api_msg:accept_function(): fix typo

--- src/api/api_msg.c
+++ src/api/api_msg.c
@@ -494,7 +494,7 @@ accept_function(void *arg, struct tcp_pcb *newpcb, err_t err)
     return ERR_VAL;
   }
 
-  LWIP_DEBUGF(API_MSG_DEBUG, ("accept_function: newpcb->tate: %s\n", tcp_debug_state_str(newpcb->state)));
+  LWIP_DEBUGF(API_MSG_DEBUG, ("accept_function: newpcb->state: %s\n", tcp_debug_state_str(newpcb->state)));
 
   /* We have to set the callback here even though
    * the new socket is unknown. newconn->socket is marked as -1. */
