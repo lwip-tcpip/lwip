20fd8449355f31bfe77df3427c9ad0bb00a76fa0
goldsimon <goldsimon@gmx.de>
2017-05-11 21:26:34 +0200

    fixed typo in docs

--- src/core/ipv4/ip4.c
+++ src/core/ipv4/ip4.c
@@ -123,7 +123,7 @@ ip4_set_default_multicast_netif(struct netif* default_multicast_netif)
 #ifdef LWIP_HOOK_IP4_ROUTE_SRC
 /**
  * Source based IPv4 routing must be fully implemented in
- * LWIP_HOOK_IP4_ROUTE_SRC(). This function only provides he parameters.
+ * LWIP_HOOK_IP4_ROUTE_SRC(). This function only provides the parameters.
  */
 struct netif *
 ip4_route_src(const ip4_addr_t *dest, const ip4_addr_t *src)
