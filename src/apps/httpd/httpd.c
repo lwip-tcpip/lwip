07cc25a9df991eb2fd7600e988345b17c5475a6c
goldsimon <goldsimon@gmx.de>
2017-06-13 21:53:35 +0200

    httpd: remove unnecessary "\0" suffix for CRLF string constant (see bug #51232)

--- src/apps/httpd/httpd.c
+++ src/apps/httpd/httpd.c
@@ -944,7 +944,7 @@ get_http_headers(struct http_state *hs, const char *uri)
       hs->handle->len);
     len = strlen(hs->hdr_content_len);
     if (len <= LWIP_HTTPD_MAX_CONTENT_LEN_SIZE - LWIP_HTTPD_MAX_CONTENT_LEN_OFFSET) {
-      SMEMCPY(&hs->hdr_content_len[len], CRLF "\0", 3);
+      SMEMCPY(&hs->hdr_content_len[len], CRLF, 3);
       hs->hdrs[HDR_STRINGS_IDX_CONTENT_LEN_NR] = hs->hdr_content_len;
     } else {
       add_content_len = 0;
