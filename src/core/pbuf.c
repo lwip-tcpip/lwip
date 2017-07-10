57b1471254b2f66de71c390bca82a587d2f82f47
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2017-07-10 09:40:21 +0200

    Avoid return in case statement, makes function control flow equal to other case statement paths

--- src/core/pbuf.c
+++ src/core/pbuf.c
@@ -227,7 +227,7 @@ pbuf_alloc(pbuf_layer layer, u16_t length, pbuf_type type)
   switch (type) {
   case PBUF_REF: /* fall through */
   case PBUF_ROM:
-    return pbuf_alloc_reference(NULL, length, type);
+    p = pbuf_alloc_reference(NULL, length, type);
   case PBUF_POOL:
     {
       struct pbuf *q, *last;
