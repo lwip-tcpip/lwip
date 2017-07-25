9b06d71aebf692de53664dadff836c4c412c1ec6
Matthias Hofmann <matthias.hofmann@diehl.com>
2017-07-25 14:32:21 +0200

    Fix using DHCP IP configuration after lease time is expired.
    Signed-off-by: goldsimon <goldsimon@gmx.de>

--- src/core/ipv4/dhcp.c
+++ src/core/ipv4/dhcp.c
@@ -1963,7 +1963,8 @@ dhcp_supplied_address(const struct netif *netif)
 {
   if ((netif != NULL) && (netif_dhcp_data(netif) != NULL)) {
     struct dhcp* dhcp = netif_dhcp_data(netif);
-    return (dhcp->state == DHCP_STATE_BOUND) || (dhcp->state == DHCP_STATE_RENEWING);
+    return (dhcp->state == DHCP_STATE_BOUND) || (dhcp->state == DHCP_STATE_RENEWING) ||
+           (dhcp->state == DHCP_STATE_REBINDING);
   }
   return 0;
 }
