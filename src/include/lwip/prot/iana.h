aaee7ed1e79b68e8ae77990630449f39ea4a8ad7
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2017-08-15 13:40:37 +0200

    Improve documentation in iana.h

--- src/include/lwip/prot/iana.h
+++ src/include/lwip/prot/iana.h
@@ -1,6 +1,9 @@
 /**
  * @file
  * IANA assigned numbers (RFC 1700 and successors)
+ *
+ * @defgroup iana IANA assigned numbers
+ * @ingroup infrastructure
  */
 
 /*
@@ -38,26 +41,53 @@
 #ifndef LWIP_HDR_PROT_IANA_H
 #define LWIP_HDR_PROT_IANA_H
 
-/* Hardware types */
-#define LWIP_IANA_HWTYPE_ETHERNET                     1
-
-/* Port numbers (https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.txt) */
-#define LWIP_IANA_PORT_SMTP                           25
-#define LWIP_IANA_PORT_DHCP_SERVER                    67
-#define LWIP_IANA_PORT_DHCP_CLIENT                    68
-#define LWIP_IANA_PORT_TFTP                           69
-#define LWIP_IANA_PORT_HTTP                           80
-#define LWIP_IANA_PORT_SNTP                           123
-#define LWIP_IANA_PORT_NETBIOS                        137
-#define LWIP_IANA_PORT_SNMP                           161
-#define LWIP_IANA_PORT_SNMP_TRAP                      162
-#define LWIP_IANA_PORT_HTTPS                          443
-#define LWIP_IANA_PORT_SMTPS                          465
-#define LWIP_IANA_PORT_MQTT                           1883
-#define LWIP_IANA_PORT_MDNS                           5353
-#define LWIP_IANA_PORT_SEQURE_MQTT                    8883
+/**
+ * @ingroup iana
+ * Hardware types
+ */
+enum lwip_iana_hwtype {
+  /** Ethernet */
+  LWIP_IANA_HWTYPE_ETHERNET = 1
+};
 
 /**
+ * @ingroup iana
+ * Port numbers
+ * https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.txt
+ */
+enum lwip_iana_port_number {
+  /** SMTP */
+  LWIP_IANA_PORT_SMTP        = 25,
+  /** DHCP server */
+  LWIP_IANA_PORT_DHCP_SERVER = 67,
+  /** DHCP client */
+  LWIP_IANA_PORT_DHCP_CLIENT = 68,
+  /** TFTP */
+  LWIP_IANA_PORT_TFTP        = 69,
+  /** HTTP */
+  LWIP_IANA_PORT_HTTP        = 80,
+  /** SNTP */
+  LWIP_IANA_PORT_SNTP        = 123,
+  /** NETBIOS */
+  LWIP_IANA_PORT_NETBIOS     = 137,
+  /** SNMP */
+  LWIP_IANA_PORT_SNMP        = 161,
+  /** SNMP traps */
+  LWIP_IANA_PORT_SNMP_TRAP   = 162,
+  /** HTTPS */
+  LWIP_IANA_PORT_HTTPS       = 443,
+  /** SMTPS */
+  LWIP_IANA_PORT_SMTPS       = 465,
+  /** MQTT */
+  LWIP_IANA_PORT_MQTT        = 1883,
+  /** MDNS */
+  LWIP_IANA_PORT_MDNS        = 5353,
+  /** Secure MQTT */
+  LWIP_IANA_PORT_SEQURE_MQTT = 8883
+};
+
+/**
+ * @ingroup iana
  * A list of often ethtypes (although lwIP does not use all of them).
  * https://www.iana.org/assignments/ieee-802-numbers/ieee-802-numbers.xhtml#ieee-802-numbers-1
  */
