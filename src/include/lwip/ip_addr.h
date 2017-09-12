7ba479e482dab29771d308388e0b6db25d6d7f10
goldsimon <goldsimon@gmx.de>
2017-09-12 21:51:50 +0200

    Try to fix clang runtime errors during unit tests (see bug #51652)

--- src/include/lwip/ip_addr.h
+++ src/include/lwip/ip_addr.h
@@ -201,9 +201,9 @@ extern const ip_addr_t ip_addr_any_type;
   ip6_addr_cmp(ip_2_ip6(addr1), ip_2_ip6(addr2)) : \
   ip4_addr_cmp(ip_2_ip4(addr1), ip_2_ip4(addr2))))
 /** @ingroup ipaddr */
-#define ip_addr_isany(ipaddr)        ((IP_IS_V6(ipaddr)) ? \
+#define ip_addr_isany(ipaddr)        (((ipaddr) == NULL) ? 1 : ((IP_IS_V6(ipaddr)) ? \
   ip6_addr_isany(ip_2_ip6(ipaddr)) : \
-  ip4_addr_isany(ip_2_ip4(ipaddr)))
+  ip4_addr_isany(ip_2_ip4(ipaddr))))
 /** @ingroup ipaddr */
 #define ip_addr_isany_val(ipaddr)        ((IP_IS_V6_VAL(ipaddr)) ? \
   ip6_addr_isany_val(*ip_2_ip6(&(ipaddr))) : \
