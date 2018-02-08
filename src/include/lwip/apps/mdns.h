f201d261b2998c1303a5bef6c3d297d045d3fa81
goldsimon <goldsimon@gmx.de>
2018-02-08 12:28:04 +0100

    Revert "MDNS send probes to verify domain before use"
    This reverts commit d6e58d02a619813a4187ab53ab0ffc2f0b864ad7.
    Erik seems to have commit this by accident. Let's discuss this first
    (see patch #9555)

--- src/include/lwip/apps/mdns.h
+++ src/include/lwip/apps/mdns.h
@@ -66,8 +66,6 @@ err_t mdns_resp_del_service(struct netif *netif, s8_t slot);
 
 err_t mdns_resp_add_service_txtitem(struct mdns_service *service, const char *txt, u8_t txt_len);
 
-void mdns_resp_start(struct netif *netif);
-void mdns_resp_restart(struct netif *netif);
 void mdns_resp_announce(struct netif *netif);
 
 /**
