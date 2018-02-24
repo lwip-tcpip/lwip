56124bf53bd720d40c392599ba554e90a86c7ace
goldsimon <goldsimon@gmx.de>
2018-02-24 14:43:51 +0100

    Add check in init.c: altcp does not work with LWIP_EVENT_API

--- src/core/init.c
+++ src/core/init.c
@@ -193,6 +193,9 @@ PACK_STRUCT_END
 #if (LWIP_TCP && ((LWIP_EVENT_API && LWIP_CALLBACK_API) || (!LWIP_EVENT_API && !LWIP_CALLBACK_API)))
 #error "One and exactly one of LWIP_EVENT_API and LWIP_CALLBACK_API has to be enabled in your lwipopts.h"
 #endif
+#if (LWIP_ALTCP && LWIP_EVENT_API)
+#error "The application layered tcp API does not work with LWIP_EVENT_API"
+#endif
 #if (MEM_LIBC_MALLOC && MEM_USE_POOLS)
 #error "MEM_LIBC_MALLOC and MEM_USE_POOLS may not both be simultaneously enabled in your lwipopts.h"
 #endif
