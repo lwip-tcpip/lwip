lwip-tcpip/lwip

commit 30a2283993cf80f64a0786d90bce3f50d5a915b1

Move netifapi private definitions to priv/api_msg.h

src/include/lwip/priv/api_msg.h
@@ -214,6 +214,46 @@ void lwip_netconn_do_gethostbyname(void *arg);
 struct netconn* netconn_alloc(enum netconn_type t, netconn_callback callback);
 void netconn_free(struct netconn *conn);
 
+/* netifapi related lwIP internal definitions */
+
+#if LWIP_MPU_COMPATIBLE
+#define NETIFAPI_IPADDR_DEF(type, m)  type m
+#else /* LWIP_MPU_COMPATIBLE */
+#define NETIFAPI_IPADDR_DEF(type, m)  const type * m
+#endif /* LWIP_MPU_COMPATIBLE */
+
+typedef void (*netifapi_void_fn)(struct netif *netif);
+typedef err_t (*netifapi_errt_fn)(struct netif *netif);
+
+struct netifapi_msg {
+  struct tcpip_api_call_data call;
+  struct netif *netif;
+  union {
+    struct {
+#if LWIP_IPV4
+      NETIFAPI_IPADDR_DEF(ip4_addr_t, ipaddr);
+      NETIFAPI_IPADDR_DEF(ip4_addr_t, netmask);
+      NETIFAPI_IPADDR_DEF(ip4_addr_t, gw);
+#endif /* LWIP_IPV4 */
+      void *state;
+      netif_init_fn init;
+      netif_input_fn input;
+    } add;
+    struct {
+      netifapi_void_fn voidfunc;
+      netifapi_errt_fn errtfunc;
+    } common;
+    struct {
+#if LWIP_MPU_COMPATIBLE
+      char name[NETIF_NAMESIZE];
+#else /* LWIP_MPU_COMPATIBLE */
+      char *name;
+#endif /* LWIP_MPU_COMPATIBLE */
+      u8_t index;
+    } ifs;
+  } msg;
+};
+
 #ifdef __cplusplus
 }
 #endif

src/include/lwip/netifapi.h
@@ -41,49 +41,11 @@
 #include "lwip/dhcp.h"
 #include "lwip/autoip.h"
 #include "lwip/priv/tcpip_priv.h"
+#include "lwip/priv/api_msg.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#if LWIP_MPU_COMPATIBLE
-#define NETIFAPI_IPADDR_DEF(type, m)  type m
-#else /* LWIP_MPU_COMPATIBLE */
-#define NETIFAPI_IPADDR_DEF(type, m)  const type * m
-#endif /* LWIP_MPU_COMPATIBLE */
-
-typedef void (*netifapi_void_fn)(struct netif *netif);
-typedef err_t (*netifapi_errt_fn)(struct netif *netif);
-
-struct netifapi_msg {
-  struct tcpip_api_call_data call;
-  struct netif *netif;
-  union {
-    struct {
-#if LWIP_IPV4
-      NETIFAPI_IPADDR_DEF(ip4_addr_t, ipaddr);
-      NETIFAPI_IPADDR_DEF(ip4_addr_t, netmask);
-      NETIFAPI_IPADDR_DEF(ip4_addr_t, gw);
-#endif /* LWIP_IPV4 */
-      void *state;
-      netif_init_fn init;
-      netif_input_fn input;
-    } add;
-    struct {
-      netifapi_void_fn voidfunc;
-      netifapi_errt_fn errtfunc;
-    } common;
-    struct {
-#if LWIP_MPU_COMPATIBLE
-      char name[NETIF_NAMESIZE];
-#else /* LWIP_MPU_COMPATIBLE */
-      char *name;
-#endif /* LWIP_MPU_COMPATIBLE */
-      u8_t index;
-    } ifs;
-  } msg;
-};
-
 
 /* API for application */
 err_t netifapi_netif_add(struct netif *netif,
@@ -105,17 +67,29 @@ err_t netifapi_netif_name_to_index(const char *name, u8_t *index);
 /** @ingroup netifapi_netif */
 err_t netifapi_netif_index_to_name(u8_t index, char *name);
 
-/** @ingroup netifapi_netif */
+/** @ingroup netifapi_netif
+  * @see netif_remove()
+  */
 #define netifapi_netif_remove(n)        netifapi_netif_common(n, netif_remove, NULL)
-/** @ingroup netifapi_netif */
+/** @ingroup netifapi_netif
+  * @see netif_set_up()
+  */
 #define netifapi_netif_set_up(n)        netifapi_netif_common(n, netif_set_up, NULL)
-/** @ingroup netifapi_netif */
+/** @ingroup netifapi_netif
+  * @see netif_set_down()
+  */
 #define netifapi_netif_set_down(n)      netifapi_netif_common(n, netif_set_down, NULL)
-/** @ingroup netifapi_netif */
+/** @ingroup netifapi_netif
+  * @see netif_set_default()
+  */
 #define netifapi_netif_set_default(n)   netifapi_netif_common(n, netif_set_default, NULL)
-/** @ingroup netifapi_netif */
+/** @ingroup netifapi_netif
+  * @see netif_set_link_up()
+  */
 #define netifapi_netif_set_link_up(n)   netifapi_netif_common(n, netif_set_link_up, NULL)
-/** @ingroup netifapi_netif */
+/** @ingroup netifapi_netif
+  * @see netif_set_link_down()
+  */
 #define netifapi_netif_set_link_down(n) netifapi_netif_common(n, netif_set_link_down, NULL)
 
 /**
@@ -155,9 +129,13 @@ err_t netifapi_netif_index_to_name(u8_t index, char *name);
  * @ingroup netifapi
  * To be called from non-TCPIP threads
  */
-/** @ingroup netifapi_autoip */
+/** @ingroup netifapi_autoip
+  * @see autoip_start()
+  */
 #define netifapi_autoip_start(n)      netifapi_netif_common(n, NULL, autoip_start)
-/** @ingroup netifapi_autoip */
+/** @ingroup netifapi_autoip
+  * @see autoip_stop()
+  */
 #define netifapi_autoip_stop(n)       netifapi_netif_common(n, NULL, autoip_stop)
 
 #ifdef __cplusplus

commit 041133285628bcd7fde39a7fd8ce08e40f10f423

api_msg.h: Fix build error when LWIP_NETIF_API && !LWIP_SOCKET && !LWIP_NETCONN
netifapi related lwIP internal definitions should be guarded
by #if LWIP_NETIF_API rather than #if LWIP_NETCONN || LWIP_SOCKET.

Fix below build errors:
cc -g -DLWIP_DEBUG -Wall -pedantic -Wparentheses -Wsequence-point -Wswitch-default -Wextra -Wundef -Wshadow -Wpointer-arith -Wcast-qual -Wc++-compat -Wwrite-strings -Wold-style-definition -Wcast-align -Wmissing-prototypes -Wredundant-decls -Wnested-externs -Wunreachable-code -Wuninitialized -Wmissing-prototypes -Wredundant-decls -Waggregate-return -Wlogical-not-parentheses -Wlogical-op -Wc90-c99-compat -Wtrampolines -I. -I../../..  -I../../../../lwip/src/include -I../../../ports/unix/port/include -I../../../../mbedtls/include -Wno-redundant-decls -DLWIP_HAVE_MBEDTLS=1 -c ../../../../lwip/src/api/netifapi.c In file included from ../../../../lwip/src/api/netifapi.c:46:0:
../../../../lwip/src/include/lwip/netifapi.h:76:50: error: unknown type name 'netifapi_void_fn'; did you mean 'netif_init_fn'?
 err_t netifapi_netif_common(struct netif *netif, netifapi_void_fn voidfunc,
                                                  ^~~~~~~~~~~~~~~~
                                                  netif_init_fn
../../../../lwip/src/include/lwip/netifapi.h:77:29: error: unknown type name 'netifapi_errt_fn'; did you mean 'netif_input_fn'?
                             netifapi_errt_fn errtfunc);
                             ^~~~~~~~~~~~~~~~
                             netif_input_fn
../../../../lwip/src/api/netifapi.c: In function 'netifapi_do_netif_add':
../../../../lwip/src/api/netifapi.c:67:22: error: dereferencing pointer to incomplete type 'struct netifapi_msg'
   if (!netif_add( msg->netif,
                      ^~

Fixes: 30a2283993cf ("Move netifapi private definitions to priv/api_msg.h")
Signed-off-by: Axel Lin <axel.lin@ingics.com>

src/include/lwip/priv/api_msg.h
@@ -39,10 +39,6 @@
 
 #include "lwip/opt.h"
 
-#if LWIP_NETCONN || LWIP_SOCKET /* don't build if not configured for use in lwipopts.h */
-/* Note: Netconn API is always available when sockets are enabled -
- * sockets are implemented on top of them */
-
 #include "lwip/arch.h"
 #include "lwip/ip_addr.h"
 #include "lwip/err.h"
@@ -54,6 +50,10 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#if LWIP_NETCONN || LWIP_SOCKET /* don't build if not configured for use in lwipopts.h */
+/* Note: Netconn API is always available when sockets are enabled -
+ * sockets are implemented on top of them */
 
 #if LWIP_MPU_COMPATIBLE
 #if LWIP_NETCONN_SEM_PER_THREAD
@@ -216,6 +216,10 @@ void lwip_netconn_do_gethostbyname(void *arg);
 struct netconn* netconn_alloc(enum netconn_type t, netconn_callback callback);
 void netconn_free(struct netconn *conn);
 
+#endif /* LWIP_NETCONN || LWIP_SOCKET */
+
+#if LWIP_NETIF_API /* don't build if not configured for use in lwipopts.h */
+
 /* netifapi related lwIP internal definitions */
 
 #if LWIP_MPU_COMPATIBLE
@@ -256,10 +260,10 @@ struct netifapi_msg {
   } msg;
 };
 
+#endif /* LWIP_NETIF_API */
+
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* LWIP_NETCONN || LWIP_SOCKET */
 
 #endif /* LWIP_HDR_API_MSG_H */