lwip-tcpip/lwip

commit 288d3c2802e53ab42464a917e9fd8712f0d58b29

fixed bug #54006: Two compiler warnings on IAR when debug is enabled

src/core/ipv4/ip4.c
@@ -1106,16 +1106,16 @@ ip4_debug_print(struct pbuf *p)
                          lwip_ntohs(IPH_CHKSUM(iphdr))));
   LWIP_DEBUGF(IP_DEBUG, ("+-------------------------------+\n"));
   LWIP_DEBUGF(IP_DEBUG, ("|  %3"U16_F"  |  %3"U16_F"  |  %3"U16_F"  |  %3"U16_F"  | (src)\n",
-                         ip4_addr1_16(&iphdr->src),
-                         ip4_addr2_16(&iphdr->src),
-                         ip4_addr3_16(&iphdr->src),
-                         ip4_addr4_16(&iphdr->src)));
+                         ip4_addr1_16_val(iphdr->src),
+                         ip4_addr1_16_val(iphdr->src),
+                         ip4_addr1_16_val(iphdr->src),
+                         ip4_addr1_16_val(iphdr->src)));
   LWIP_DEBUGF(IP_DEBUG, ("+-------------------------------+\n"));
   LWIP_DEBUGF(IP_DEBUG, ("|  %3"U16_F"  |  %3"U16_F"  |  %3"U16_F"  |  %3"U16_F"  | (dest)\n",
-                         ip4_addr1_16(&iphdr->dest),
-                         ip4_addr2_16(&iphdr->dest),
-                         ip4_addr3_16(&iphdr->dest),
-                         ip4_addr4_16(&iphdr->dest)));
+                         ip4_addr1_16_val(iphdr->dest),
+                         ip4_addr1_16_val(iphdr->dest),
+                         ip4_addr1_16_val(iphdr->dest),
+                         ip4_addr1_16_val(iphdr->dest)));
   LWIP_DEBUGF(IP_DEBUG, ("+-------------------------------+\n"));
 }
 #endif /* IP_DEBUG */

commit 1fd145fbc9f096a48172170293dfaef6c3c05851

Fix trivial copy-n-paste mistake in ip4_debug_print
Fixes: 288d3c2802e5 ("fixed bug #54006: Two compiler warnings on IAR when debug is enabled")
Signed-off-by: Axel Lin <axel.lin@ingics.com>

src/core/ipv4/ip4.c
@@ -1107,15 +1107,15 @@ ip4_debug_print(struct pbuf *p)
   LWIP_DEBUGF(IP_DEBUG, ("+-------------------------------+\n"));
   LWIP_DEBUGF(IP_DEBUG, ("|  %3"U16_F"  |  %3"U16_F"  |  %3"U16_F"  |  %3"U16_F"  | (src)\n",
                          ip4_addr1_16_val(iphdr->src),
-                         ip4_addr1_16_val(iphdr->src),
-                         ip4_addr1_16_val(iphdr->src),
-                         ip4_addr1_16_val(iphdr->src)));
+                         ip4_addr2_16_val(iphdr->src),
+                         ip4_addr3_16_val(iphdr->src),
+                         ip4_addr4_16_val(iphdr->src)));
   LWIP_DEBUGF(IP_DEBUG, ("+-------------------------------+\n"));
   LWIP_DEBUGF(IP_DEBUG, ("|  %3"U16_F"  |  %3"U16_F"  |  %3"U16_F"  |  %3"U16_F"  | (dest)\n",
                          ip4_addr1_16_val(iphdr->dest),
-                         ip4_addr1_16_val(iphdr->dest),
-                         ip4_addr1_16_val(iphdr->dest),
-                         ip4_addr1_16_val(iphdr->dest)));
+                         ip4_addr2_16_val(iphdr->dest),
+                         ip4_addr3_16_val(iphdr->dest),
+                         ip4_addr4_16_val(iphdr->dest)));
   LWIP_DEBUGF(IP_DEBUG, ("+-------------------------------+\n"));
 }
 #endif /* IP_DEBUG */