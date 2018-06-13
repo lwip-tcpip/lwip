lwip-tcpip/lwip

commit 31bc2f9b207eb1d0712065f2f9b971a4e9f63dc4

fixed bug #51908: PPPOS uses PBUF_POOL in transmit pathway
Signed-off-by: Simon Goldschmidt <goldsimon@gmx.de>

src/netif/ppp/pppos.c
@@ -208,7 +208,7 @@ pppos_write(ppp_pcb *ppp, void *ctx, struct pbuf *p)
   LWIP_UNUSED_ARG(ppp);
 
   /* Grab an output buffer. */
-  nb = pbuf_alloc(PBUF_RAW, 0, PBUF_POOL);
+  nb = pbuf_alloc(PBUF_RAW, 0, PBUF_RAM);
   if (nb == NULL) {
     PPPDEBUG(LOG_WARNING, ("pppos_write[%d]: alloc fail\n", ppp->netif->num));
     LINK_STATS_INC(link.memerr);
@@ -257,7 +257,7 @@ pppos_netif_output(ppp_pcb *ppp, void *ctx, struct pbuf *pb, u16_t protocol)
   LWIP_UNUSED_ARG(ppp);
 
   /* Grab an output buffer. */
-  nb = pbuf_alloc(PBUF_RAW, 0, PBUF_POOL);
+  nb = pbuf_alloc(PBUF_RAW, 0, PBUF_RAM);
   if (nb == NULL) {
     PPPDEBUG(LOG_WARNING, ("pppos_netif_output[%d]: alloc fail\n", ppp->netif->num));
     LINK_STATS_INC(link.memerr);

src/netif/ppp/mppe.c
@@ -201,7 +201,7 @@ mppe_compress(ppp_pcb *pcb, ppp_mppe_state *state, struct pbuf **pb, u16_t proto
 	/* TCP stack requires that we don't change the packet payload, therefore we copy
 	 * the whole packet before encryption.
 	 */
-	np = pbuf_alloc(PBUF_RAW, MPPE_OVHD + sizeof(protocol) + (*pb)->tot_len, PBUF_POOL);
+	np = pbuf_alloc(PBUF_RAW, MPPE_OVHD + sizeof(protocol) + (*pb)->tot_len, PBUF_RAM);
 	if (!np) {
 		return ERR_MEM;
 	}

src/netif/ppp/vj.c
@@ -202,7 +202,7 @@ vj_compress_tcp(struct vjcompress *comp, struct pbuf **pb)
 
   /* TCP stack requires that we don't change the packet payload, therefore we copy
    * the whole packet before compression. */
-  np = pbuf_clone(PBUF_RAW, PBUF_POOL, *pb);
+  np = pbuf_clone(PBUF_RAW, PBUF_RAM, *pb);
   if (!np) {
     return (TYPE_IP);
   }

commit 3f2227f04c6361a7eb091a772e1b4fdbdee343db

Fix pppos after changing PBUF_POOL to PBUF_RAM
This partly reverts 31bc2f9b which broke pppos tx as 'pppos_output_append'
implements a custom scheme of checking available pbuf size.
Added a comment why PBUF_POOL is ok for tx in this special case.
See bug #51908

Signed-off-by: Simon Goldschmidt <goldsimon@gmx.de>

src/netif/ppp/pppos.c
@@ -207,8 +207,10 @@ pppos_write(ppp_pcb *ppp, void *ctx, struct pbuf *p)
   err_t err;
   LWIP_UNUSED_ARG(ppp);
 
-  /* Grab an output buffer. */
-  nb = pbuf_alloc(PBUF_RAW, 0, PBUF_RAM);
+  /* Grab an output buffer. Using PBUF_POOL here for tx is ok since the pbuf
+     gets freed by 'pppos_output_last' before this function returns and thus
+     cannot starve rx. */
+  nb = pbuf_alloc(PBUF_RAW, 0, PBUF_POOL);
   if (nb == NULL) {
     PPPDEBUG(LOG_WARNING, ("pppos_write[%d]: alloc fail\n", ppp->netif->num));
     LINK_STATS_INC(link.memerr);
@@ -256,8 +258,10 @@ pppos_netif_output(ppp_pcb *ppp, void *ctx, struct pbuf *pb, u16_t protocol)
   err_t err;
   LWIP_UNUSED_ARG(ppp);
 
-  /* Grab an output buffer. */
-  nb = pbuf_alloc(PBUF_RAW, 0, PBUF_RAM);
+  /* Grab an output buffer. Using PBUF_POOL here for tx is ok since the pbuf
+     gets freed by 'pppos_output_last' before this function returns and thus
+     cannot starve rx. */
+  nb = pbuf_alloc(PBUF_RAW, 0, PBUF_POOL);
   if (nb == NULL) {
     PPPDEBUG(LOG_WARNING, ("pppos_netif_output[%d]: alloc fail\n", ppp->netif->num));
     LINK_STATS_INC(link.memerr);