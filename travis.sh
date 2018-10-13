31545ed7874c000922b557c4a11e601a3c5bce11
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2018-10-13 11:06:00 +0200

    Getting CMake build running on travis

--- travis.sh
+++ travis.sh
@@ -8,16 +8,24 @@ if [ $ERR != 0 ]; then
        echo "unittests build failed"
        exit 33
 fi
+# Build test using make, this tests the Makefile toolchain
 make check -j 4
 
-cd ../../../../
 
+# Build example_app using cmake, this tests the CMake toolchain
+cd ../../../../
+# Copy lwipcfg for example app
+cp contrib/examples/example_app/lwipcfg.h.example contrib/examples/example_app/lwipcfg.h
+
+# Generate CMake
 mkdir build
 cd build
 cmake ..
+
+# Build CMake
 ERR=$?
 if [ $ERR != 0 ]; then
-       echo "cmake failed"
+       echo "cmake build failed"
        exit 33
 fi
-cmake --build .
+cmake --build . -- -j 4
