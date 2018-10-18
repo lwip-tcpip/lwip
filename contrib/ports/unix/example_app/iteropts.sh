5405c3d97da2a509fae894d6dae1a14541ac366e
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2018-10-18 22:36:59 +0200

    Cleanup iteropts.sh output a bit

--- contrib/ports/unix/example_app/iteropts.sh
+++ contrib/ports/unix/example_app/iteropts.sh
@@ -10,17 +10,17 @@ echo Starting Iteropts run >> $LOGFILE
 for f in $EXAPPDIR/test_configs/*.h
 do
     echo Cleaning...
-    make clean
+    make clean > /dev/null
     BUILDLOG=$(basename "$f" ".h").log
     echo testing $f
     echo testing $f >> $LOGFILE
     rm $EXAPPDIR/lwipopts_test.h
     # cat the file to update its timestamp
     cat $f > $EXAPPDIR/lwipopts_test.h
-    make TESTFLAGS=-DLWIP_OPTTEST_FILE -j 8 &> $BUILDLOG 2>&1 || (RETVAL=1; echo file $f failed >> $LOGFILE; echo ++++++++ $f FAILED +++++++; cat $BUILDLOG)
+    make TESTFLAGS=-DLWIP_OPTTEST_FILE -j 8 1> $BUILDLOG 2>&1 || (RETVAL=1; echo file $f failed >> $LOGFILE; echo ++++++++ $f FAILED +++++++; cat $BUILDLOG)
     echo test $f done >> $LOGFILE
 done
 echo done, cleaning
-make clean
+make clean > /dev/null
 popd
 exit $RETVAL
