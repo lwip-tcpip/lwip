398333da9a2abe654cdaeea684246ec70b1af2ef
Simon Goldschmidt <goldsimon@gmx.de>
2018-10-24 20:39:08 +0200

    snmp: avoid NULL pointer dereference, fix return values

--- src/apps/snmp/snmp_scalar.c
+++ src/apps/snmp/snmp_scalar.c
@@ -193,7 +193,7 @@ snmp_scalar_array_get_next_instance(const u32_t *root_oid, u8_t root_oid_len, st
 static s16_t
 snmp_scalar_array_get_value(struct snmp_node_instance *instance, void *value)
 {
-  s16_t result = SNMP_ERR_NOSUCHINSTANCE;
+  s16_t result = -1;
   const struct snmp_scalar_array_node *array_node = (const struct snmp_scalar_array_node *)(const void *)instance->node;
   const struct snmp_scalar_array_node_def *array_node_def = (const struct snmp_scalar_array_node_def *)instance->reference.const_ptr;
 
@@ -206,7 +206,7 @@ snmp_scalar_array_get_value(struct snmp_node_instance *instance, void *value)
 static snmp_err_t
 snmp_scalar_array_set_test(struct snmp_node_instance *instance, u16_t value_len, void *value)
 {
-  snmp_err_t result = SNMP_ERR_NOSUCHINSTANCE;
+  snmp_err_t result = SNMP_ERR_NOTWRITABLE;
   const struct snmp_scalar_array_node *array_node = (const struct snmp_scalar_array_node *)(const void *)instance->node;
   const struct snmp_scalar_array_node_def *array_node_def = (const struct snmp_scalar_array_node_def *)instance->reference.const_ptr;
 
@@ -219,7 +219,7 @@ snmp_scalar_array_set_test(struct snmp_node_instance *instance, u16_t value_len,
 static snmp_err_t
 snmp_scalar_array_set_value(struct snmp_node_instance *instance, u16_t value_len, void *value)
 {
-  snmp_err_t result = SNMP_ERR_NOSUCHINSTANCE;
+  snmp_err_t result = SNMP_ERR_NOTWRITABLE;
   const struct snmp_scalar_array_node *array_node = (const struct snmp_scalar_array_node *)(const void *)instance->node;
   const struct snmp_scalar_array_node_def *array_node_def = (const struct snmp_scalar_array_node_def *)instance->reference.const_ptr;
 
