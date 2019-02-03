049cae841df1b5bf61f99b80900f03fb7fdac630
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2019-02-03 09:36:44 +0100

    More cleanups to travis.sh - run all test even if some fail
    Try to get maximum on information in one build run

--- travis.sh
+++ travis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+RETVAL=0
+
 cd contrib/ports/unix/check
 
 #build and run unit tests
@@ -10,8 +12,8 @@ make check -j 4
 ERR=$?
 echo Return value from unittests: $ERR
 if [ $ERR != 0 ]; then
-       echo "unittests build failed"
-       exit 33
+       echo "++++++++++++++++++++++++++++++ unittests build failed"
+       RETVAL=1
 fi
 
 # Build example_app using cmake, this tests the CMake toolchain
@@ -26,8 +28,8 @@ cd build
 ERR=$?
 echo Return value from cmake generate: $ERR
 if [ $ERR != 0 ]; then
-       echo "cmake GENERATE failed"
-       exit 33
+       echo "++++++++++++++++++++++++++++++ cmake GENERATE failed"
+       RETVAL=1
 fi
 
 # Build CMake
@@ -35,8 +37,8 @@ fi
 ERR=$?
 echo Return value from build: $ERR
 if [ $ERR != 0 ]; then
-       echo "cmake build failed"
-       exit 33
+       echo "++++++++++++++++++++++++++++++ cmake build failed"
+       RETVAL=1
 fi
 
 # Build docs
@@ -44,8 +46,8 @@ fi
 ERR=$?
 echo Return value from lwipdocs: $ERR
 if [ $ERR != 0 ]; then
-       echo "lwIP documentation failed"
-       exit 33
+       echo "++++++++++++++++++++++++++++++ lwIP documentation failed"
+       RETVAL=1
 fi
 
 # Test different lwipopts.h
@@ -55,6 +57,9 @@ cd contrib/ports/unix/example_app
 ERR=$?
 echo Return value from iteropts: $ERR
 if [ $ERR != 0 ]; then
-       echo "lwIP iteropts test failed"
-       exit 33
+       echo "++++++++++++++++++++++++++++++ lwIP iteropts test failed"
+       RETVAL=1
 fi
+
+echo Exit value: $RETVAL
+exit $RETVAL
