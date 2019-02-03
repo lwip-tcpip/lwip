8a08c4cd4f280573bbc8865434f985f95cdd9d75
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2019-02-03 09:03:22 +0100

    Cleanup travis build script

--- travis.sh
+++ travis.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 cd contrib/ports/unix/check
+
 #build and run unit tests
 make clean all
+
+# Build test using make, this tests the Makefile toolchain
+make check -j 4
 ERR=$?
+echo Return value from unittests: $ERR
 if [ $ERR != 0 ]; then
        echo "unittests build failed"
        exit 33
 fi
-# Build test using make, this tests the Makefile toolchain
-make check -j 4
-
 
 # Build example_app using cmake, this tests the CMake toolchain
 cd ../../../../
@@ -21,31 +23,37 @@ cp contrib/examples/example_app/lwipcfg.h.travis contrib/examples/example_app/lw
 mkdir build
 cd build
 /usr/local/bin/cmake .. -G Ninja
-
-# Build CMake
 ERR=$?
+echo Return value from cmake generate: $ERR
 if [ $ERR != 0 ]; then
        echo "cmake GENERATE failed"
        exit 33
 fi
+
+# Build CMake
 /usr/local/bin/cmake --build .
 ERR=$?
+echo Return value from build: $ERR
 if [ $ERR != 0 ]; then
        echo "cmake build failed"
        exit 33
 fi
 
+# Build docs
 /usr/local/bin/cmake --build . --target lwipdocs
 ERR=$?
+echo Return value from lwipdocs: $ERR
 if [ $ERR != 0 ]; then
        echo "lwIP documentation failed"
        exit 33
 fi
 
+# Test different lwipopts.h
 cd ..
 cd contrib/ports/unix/example_app
 ./iteropts.sh
 ERR=$?
+echo Return value from iteropts: $ERR
 if [ $ERR != 0 ]; then
        echo "lwIP iteropts test failed"
        exit 33
