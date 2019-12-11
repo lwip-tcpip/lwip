0caacf82b4a787f7811d94da924abd024c183fe1
Simon Goldschmidt <goldsimon@gmx.de>
2019-12-11 19:49:50 +0100

    netif_add: zero-init netif->acd_list

--- src/core/netif.c
+++ src/core/netif.c
@@ -369,6 +369,9 @@ netif_add(struct netif *netif,
   netif->num = netif_num;
   netif->input = input;
 
+#if LWIP_ACD
+  netif->acd_list = NULL;
+#endif /* LWIP_ACD */
   NETIF_RESET_HINTS(netif);
 #if ENABLE_LOOPBACK && LWIP_LOOPBACK_MAX_PBUFS
   netif->loop_cnt_current = 0;
