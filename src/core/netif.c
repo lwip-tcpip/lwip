2f8886794ffd65029693c95dc445a9e696363922
Simon Goldschmidt <goldsimon@gmx.de>
2019-12-11 20:42:36 +0100

    netif_find: check if atoi means '0' or error
    Since atoi() returns 0 on error, we need to check if name[2] is '0'.
    If it's not, atoi() failed.

--- src/core/netif.c
+++ src/core/netif.c
@@ -1714,6 +1714,10 @@ netif_find(const char *name)
   }
 
   num = (u8_t)atoi(&name[2]);
+  if (!num && (name[2] != 0)) {
+    /* this means atoi has failed */
+    return NULL;
+  }
 
   NETIF_FOREACH(netif) {
     if (num == netif->num &&
