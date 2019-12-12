555812dcec38c9a2ef1ef9b31816291549fbf9f8
Axel Lin <axel.lin@ingics.com>
2019-12-12 09:23:11 +0800

    netif_find: correctly check if atoi means '0' or error
    Fixes: 4528215c99ba ("netif_find: check if atoi means '0' or error")
    Signed-off-by: Axel Lin <axel.lin@ingics.com>

--- src/core/netif.c
+++ src/core/netif.c
@@ -1744,7 +1744,7 @@ netif_find(const char *name)
   }
 
   num = (u8_t)atoi(&name[2]);
-  if (!num && (name[2] != 0)) {
+  if (!num && (name[2] != '0')) {
     /* this means atoi has failed */
     return NULL;
   }

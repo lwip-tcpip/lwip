lwip-tcpip/lwip

commit 4528215c99bace1980218a5df3d2fe11dbe9628d

netif_find: check if atoi means '0' or error
Since atoi() returns 0 on error, we need to check if name[2] is '0'.
If it's not, atoi() failed.

src/core/netif.c
@@ -1744,6 +1744,10 @@ netif_find(const char *name)
   }
 
   num = (u8_t)atoi(&name[2]);
+  if (!num && (name[2] != 0)) {
+    /* this means atoi has failed */
+    return NULL;
+  }
 
   NETIF_FOREACH(netif) {
     if (num == netif->num &&

commit 555812dcec38c9a2ef1ef9b31816291549fbf9f8

netif_find: correctly check if atoi means '0' or error
Fixes: 4528215c99ba ("netif_find: check if atoi means '0' or error")
Signed-off-by: Axel Lin <axel.lin@ingics.com>

src/core/netif.c
@@ -1744,7 +1744,7 @@ netif_find(const char *name)
   }
 
   num = (u8_t)atoi(&name[2]);
-  if (!num && (name[2] != 0)) {
+  if (!num && (name[2] != '0')) {
     /* this means atoi has failed */
     return NULL;
   }