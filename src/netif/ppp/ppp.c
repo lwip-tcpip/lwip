3b5775071531237ea13e3b41595c03d085fb5cfe
Sylvain Rochet <gradator@gradator.net>
2020-10-20 04:15:25 +0200

    PPP: remove LWIP_ASSERT_CORE_LOCKED on ppp_set_auth function
    We do not have equivalents in PPPAPI for ppp_set_* functions because
    calling them only makes sense while session is disconnected, furthermore
    they are only setting structure members of the session configuration.

--- src/netif/ppp/ppp.c
+++ src/netif/ppp/ppp.c
@@ -216,7 +216,6 @@ static err_t ppp_netif_output(struct netif *netif, struct pbuf *pb, u16_t protoc
 /***********************************/
 #if PPP_AUTH_SUPPORT
 void ppp_set_auth(ppp_pcb *pcb, u8_t authtype, const char *user, const char *passwd) {
-  LWIP_ASSERT_CORE_LOCKED();
 #if PAP_SUPPORT
   pcb->settings.refuse_pap = !(authtype & PPPAUTHTYPE_PAP);
 #endif /* PAP_SUPPORT */
