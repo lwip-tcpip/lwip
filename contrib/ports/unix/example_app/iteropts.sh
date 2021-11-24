3636b3e2d1832162fa21241d32eec8f1515bc6fe
Florian La Roche <florian.laroche@gmail.com>
2021-11-24 23:04:14 +0100

    iteropts.sh: Fix warning on first run
    Running iteropts.sh there is a warning on the first
    cycle that lwipopts_test.h cannot be removed.

--- contrib/ports/unix/example_app/iteropts.sh
+++ contrib/ports/unix/example_app/iteropts.sh
@@ -14,7 +14,7 @@ do
     BUILDLOG=$(basename "$f" ".h").log
     echo testing $f
     echo testing $f >> $LOGFILE
-    rm $EXAPPDIR/lwipopts_test.h
+    rm -f $EXAPPDIR/lwipopts_test.h
     # cat the file to update its timestamp
     cat $f > $EXAPPDIR/lwipopts_test.h
     make TESTFLAGS="-DLWIP_OPTTEST_FILE -Wno-documentation" -j 4 1> $BUILDLOG 2>&1
