17413c28b464345e2c09958ef2a90b5c2e47c57c
Erik Ekman <erik@kryo.se>
2021-11-27 20:36:09 +0100

    httpd: Add documentation for http_set_cgi_handlers

--- src/include/lwip/apps/httpd.h
+++ src/include/lwip/apps/httpd.h
@@ -95,6 +95,7 @@ typedef struct
     tCGIHandler pfnCGIHandler;
 } tCGI;
 
+/** Set the array of cgi handlers. */
 void http_set_cgi_handlers(const tCGI *pCGIs, int iNumHandlers);
 
 #endif /* LWIP_HTTPD_CGI */
