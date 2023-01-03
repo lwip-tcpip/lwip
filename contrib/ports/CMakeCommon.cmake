3fe8d2fc43a9b69f7ed28c63d44a7744f9c0def9
Simon Goldschmidt <goldsimon@gmx.de>
2023-01-03 21:34:05 +0100

    get cmake to compile under windows with ms cl compiler
    Signed-off-by: Simon Goldschmidt <goldsimon@gmx.de>

--- contrib/ports/CMakeCommon.cmake
+++ contrib/ports/CMakeCommon.cmake
@@ -122,7 +122,7 @@ if(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
     set(LWIP_COMPILER_FLAGS
         $<$<CONFIG:Debug>:/Od>
         $<$<CONFIG:Release>:/Ox>
-        /Wall
+        /W4
         /WX
     )
 endif()
