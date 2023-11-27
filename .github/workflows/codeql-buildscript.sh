dc6d9d9169fd14700cc6f20eb772f2b2bba1b4c0
Brian <89487381+b4yuan@users.noreply.github.com>
2023-11-27 11:36:47 -0500

    Update codeql-buildscript.sh
    Adding install dependencies step

--- .github/workflows/codeql-buildscript.sh
+++ .github/workflows/codeql-buildscript.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo apt-get install check ninja-build doxygen
 cp contrib/examples/example_app/lwipcfg.h.ci contrib/examples/example_app/lwipcfg.h
 make -C contrib/ports/unix/check
 mkdir build && cd build && cmake .. -G Ninja && cmake --build .
