2e023ebb972c9c330d12fa189ba48f9bd81637f3
Sebastian Huber <sebastian.huber@embedded-brains.de>
2025-05-29 04:50:34 +0200

    unit/def: Test string and memory functions
    Yields 100% line and branch coverage for the def.c module.

--- test/unit/core/test_def.c
+++ test/unit/core/test_def.c
@@ -28,6 +28,71 @@ def_check_range_untouched(const char *buf, size_t len)
   }
 }
 
+START_TEST(test_def_lwip_strnstr)
+{
+  const char *buffer = "abc";
+
+  LWIP_UNUSED_ARG(_i);
+
+  fail_unless(lwip_strnstr(buffer, "", 3) == buffer);
+  fail_unless(lwip_strnstr(buffer, "bc", 3) == buffer + 1);
+  fail_unless(lwip_strnstr(buffer, "bx", 3) == NULL);
+  fail_unless(lwip_strnstr(buffer, "x", 3) == NULL);
+}
+END_TEST
+
+START_TEST(test_def_lwip_strnistr)
+{
+  const char *buffer = "aBC";
+
+  LWIP_UNUSED_ARG(_i);
+
+  fail_unless(lwip_strnistr(buffer, "", 3) == buffer);
+  fail_unless(lwip_strnistr(buffer, "bc", 3) == buffer + 1);
+  fail_unless(lwip_strnistr(buffer, "bx", 3) == NULL);
+  fail_unless(lwip_strnistr(buffer, "x", 3) == NULL);
+}
+END_TEST
+
+START_TEST(test_def_lwip_stricmp)
+{
+  LWIP_UNUSED_ARG(_i);
+
+  fail_unless(lwip_stricmp("", "") == 0);
+  fail_unless(lwip_stricmp("!", "!") == 0);
+  fail_unless(lwip_stricmp("!", "{") != 0);
+  fail_unless(lwip_stricmp("{", "!") != 0);
+  fail_unless(lwip_stricmp("{", "{") == 0);
+  fail_unless(lwip_stricmp("1", "1") == 0);
+  fail_unless(lwip_stricmp("1", "2") != 0);
+  fail_unless(lwip_stricmp("a", "a") == 0);
+  fail_unless(lwip_stricmp("a", "b") != 0);
+  fail_unless(lwip_stricmp("a", "A") == 0);
+  fail_unless(lwip_stricmp("a", "b") != 0);
+}
+END_TEST
+
+START_TEST(test_def_lwip_strincmp)
+{
+  int i;
+
+  LWIP_UNUSED_ARG(_i);
+
+  for (i = 2; i < 3; ++i) {
+    fail_unless(lwip_strnicmp("", "", i) == 0);
+    fail_unless(lwip_strnicmp("0!", "0!", i) == 0);
+    fail_unless(lwip_strnicmp("0!", "0{", i) != 0);
+    fail_unless(lwip_strnicmp("0{", "0!", i) != 0);
+    fail_unless(lwip_strnicmp("0{", "0{", i) == 0);
+    fail_unless(lwip_strnicmp("01", "01", i) == 0);
+    fail_unless(lwip_strnicmp("01", "02", i) != 0);
+    fail_unless(lwip_strnicmp("0a", "0a", i) == 0);
+    fail_unless(lwip_strnicmp("0a", "0b", i) != 0);
+    fail_unless(lwip_strnicmp("0a", "0A", i) == 0);
+    fail_unless(lwip_strnicmp("0a", "0b", i) != 0);
+  }
+}
+
 static void test_def_itoa(int number, const char *expected)
 {
   char buf[TEST_BUFSIZE];
@@ -60,7 +125,13 @@ static void test_def_itoa(int number, const char *expected)
 
 START_TEST(test_def_lwip_itoa)
 {
+  char ch;
+
   LWIP_UNUSED_ARG(_i);
+
+  lwip_itoa(&ch, 0, 0);
+  lwip_itoa(&ch, 1, 0);
+  fail_unless(ch == '\0');
 
   test_def_itoa(0, "0");
   test_def_itoa(1, "1");
@@ -73,12 +144,30 @@ START_TEST(test_def_lwip_itoa)
 }
 END_TEST
 
+START_TEST(test_def_lwip_memcmp_consttime)
+{
+  char a = 'a';
+  char b = 'b';
+
+  LWIP_UNUSED_ARG(_i);
+
+  fail_unless(lwip_memcmp_consttime(NULL, NULL, 0) == 0);
+  fail_unless(lwip_memcmp_consttime(&a, &a, sizeof(a)) == 0);
+  fail_unless(lwip_memcmp_consttime(&a, &b, sizeof(a)) != 0);
+}
+END_TEST
+
 /** Create the suite including all tests for this module */
 Suite *
 def_suite(void)
 {
   testfunc tests[] = {
-    TESTFUNC(test_def_lwip_itoa)
+    TESTFUNC(test_def_lwip_strnstr),
+    TESTFUNC(test_def_lwip_strnistr),
+    TESTFUNC(test_def_lwip_stricmp),
+    TESTFUNC(test_def_lwip_strincmp),
+    TESTFUNC(test_def_lwip_itoa),
+    TESTFUNC(test_def_lwip_memcmp_consttime)
   };
   return create_suite("DEF", tests, sizeof(tests)/sizeof(testfunc), def_setup, def_teardown);
 }
